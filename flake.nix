b63e19b2ecfba1f624a252b173b47cf4b13c58ab
Mugahed Izzeldin <mugahedman@hotmail.com>
2025-09-07 22:43:01 -0600

    initial commit for nixvim

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "NixVim configuration using nixos-unstable and nvf";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nvf.url = "github:NotAShelf/nvf";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    myNeovim = nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      modules = [./nvf/default.nix];
+    };
+    mugaSystemTools = pkgs.symlinkJoin {
+      name = "mugaSystemTools";
+      paths = [pkgs.zellij myNeovim.neovim];
+    };
+  in {
+    # package output
+    packages.${system}.default = mugaSystemTools;
+
+    #  app for Neovim
+    apps.${system} = {
+      nvim = {
+        type = "app";
+        program = "${mugaSystemTools}/bin/nvim";
+      };
+      zellij = {
+        type = "app";
+        program = "${mugaSystemTools}/bin/zellij";
+      };
+    };
+    # This is the new part for nix develop
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        mugaSystemTools
+      ];
+    };
+  };
+}
