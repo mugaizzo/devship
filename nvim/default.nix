b2b2df3dd3d1bf970212d05accea7b003af2ab7a
Mugahed Izzeldin <mugahedman@hotmail.com>
2025-09-22 08:50:32 -0600

    nix.lsp.enable

--- nvim/default.nix
+++ nvim/default.nix
@@ -82,7 +82,7 @@ in {
       enableTreesitter = true;
       nix = {
         enable = true;
-        lsp.enbale = true;
+        lsp.enable = true;
         format.enable = true;
         format.package = pkgs.alejandra;
         extraDiagnostics = {
